9f60c45d3e980b820209cb9e58fb499d32da57d1
Hemant Jhalani <hj29061987@gmail.com>
2025-03-16 21:55:04 -0400

    add sql file and h2 property

---
+++ src/main/java/com/example/learning/configs/DataSourceConfig.java
@@ -0,0 +1,12 @@
+package com.example.learning.configs;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+
+    public DataSourceConfig() {
+        System.out.println("Data source config started");
+    }
+
+}
---
+++ src/main/java/com/example/learning/configs/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.example.learning.configs;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+
+   public SwaggerConfig() {
+        System.out.println("Swagger Config started");
+
+    }
+}
